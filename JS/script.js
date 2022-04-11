a8a2c6406ce4c95a9dc9d1ad1fb27090c9d7e519
Zoom2432 <vsgz2432@gmail.com>
2022-04-11 18:33:06 +0300

    v2

--- JS/script.js
+++ JS/script.js
@@ -10,12 +10,25 @@ let personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = +prompt("На сколько его оцените?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = +prompt("На сколько его оцените?", "");
+for (let i = 0; i < 2;) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+          b = prompt("На сколько его оцените?", "");
+    if (a.length != 0 && a.length <= 50 && a != null && b.length != 0 && b != null) {
+        personalMovieDB.movies[a] = b;
+        i++;
+    } else {
+        continue;
+    }
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов.");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель.");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы - киноман.");
+} else {
+    console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
